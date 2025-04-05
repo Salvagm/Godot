481bd90bebd9f98f8c937cd687f1fd02f21be6dd
Marcos Crispin Ortuzar <>
2025-04-05 16:15:17 +0200

    Push ball behaviour with bounce

--- Godogj/scripts/ballBehaviour.gd
+++ Godogj/scripts/ballBehaviour.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
+var bounce_factor = 1
+
 func _physics_process(delta):
-	move_and_collide(velocity)
+	var collision = move_and_collide(velocity)
+	if collision and collision.get_collider():
+		if collision.get_collider() is not CharacterBody2D:
+			velocity = velocity.bounce(collision.get_normal()) * bounce_factor
+		elif collision.get_collider() is CharacterBody2D:
+			velocity = Vector2.ZERO
