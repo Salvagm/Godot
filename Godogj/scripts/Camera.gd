57bfe757d3125f5f353a721c170e9b5a85e7e937
DANIEL\dcabe <d.cabero.a@gmail.com>
2025-04-06 13:02:12 +0100

    Character movement

--- Godogj/scripts/Camera.gd
+++ Godogj/scripts/Camera.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-var DistanceToPlayer = 10
+var DistanceToPlayer = Vector3(0.0, 10, -1.5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,8 +11,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var PlayerNode = get_tree().current_scene.get_node("Player")
 	var PlayerLocation = PlayerNode.transform.origin
-	var PlayerForward = -PlayerNode.transform.basis.z
-	var Cameralocation = PlayerLocation + (PlayerForward * 1.5);
-	Cameralocation.y = DistanceToPlayer;
-	
+	var Cameralocation = PlayerLocation + DistanceToPlayer;
 	transform.origin = Cameralocation
